311c49f530d8321a5e466d97d6bf5519937893b4
WilliamCDai <73041960+WilliamCDai@users.noreply.github.com>
2023-01-16 11:38:10 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -12,14 +12,17 @@ public class Player extends Rectangle{
 	public char upButton, leftButton, rightButton;
 	public boolean inAir;
 	Color colour;
+	Door destination;
 	
-	public Player(char upButton, char leftButton, char rightButton, int startX, int startY, Color colour) {
+	public Player(char upButton, char leftButton, char rightButton, int startX, int startY, Color colour, int doorX, int doorY) {
 		super(startX, startY, PlayerWidth, PlayerHeight);
 		x = startX; y = startY;
+		
 		this.upButton = upButton;
 		this.leftButton = leftButton;
 		this.rightButton = rightButton;
 		this.colour = colour;
+		destination = new Door(doorX, doorY, this.colour);
 		
 		xVelocity = 0; yVelocity = 0;
 		inAir = true;
@@ -65,16 +68,23 @@ public class Player extends Rectangle{
 		}
 	}
 	
+	public boolean atDestination() {
+		if(x < destination.x) return false;
+		if(y < destination.y) return false;
+		if(x + PlayerWidth > destination.x + Door.DOOR_WIDTH) return false;
+		if(y + PlayerHeight > destination.y + Door.DOOR_HEIGHT) return false;
+		return true;
+	}
+	
 	public void move() {
 		y += yVelocity;
-		//if(inAir) {
-			yVelocity++;
-		//}
+		yVelocity++;
 		x += xVelocity;
 	}
 	
 	public void draw(Graphics g) {
 		g.setColor(colour); // set colour to white
 		g.fillRect(x, y, PlayerWidth, PlayerHeight); // draw the rectangle with the position and dimensions
+		destination.draw(g);
 	}
 }
